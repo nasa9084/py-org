59720e2b85d6bb91508d45f8faf8dcd582c646c3
nasa9084 <nasa9084@live.jp>
2016-12-08 01:19:44 +0900

    add: some file need to modulize

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='pyorg',
+    packages=['pyorg'],
+    version='0.1',
+    description='The org-mode parser fro python',
+    author='nasa9084',
+    author_email='nasa.9084.bassclarinet@gmail.com',
+    url='http://blog.web-apps.tech',
+    keywords=['org-mode', 'emacs'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Natural Language :: Japanese',
+        'Operating System :: OS :: Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Editors',
+        'Topic :: Text Editors :: Emacs',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
