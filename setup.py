9885724b227863fda2bbbf846ea643b61bd2e164
nasa9084 <nasa9084@live.jp>
2017-01-22 22:50:11 +0900

    update: version to 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyorg',
     packages=['pyorg'],
-    version='0.1.6',
+    version='0.1.7',
     description='The org-mode parser fro python',
     author='nasa9084',
     author_email='nasa.9084.bassclarinet@gmail.com',
